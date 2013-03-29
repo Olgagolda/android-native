64c1b5948aea431d0859a05a0666713974852223
darya.tkachenko-econ <PROD\\darya.tkachenko-econ>
2013-03-29 19:49:10 +0200

    add activity, rename fullscreen activity

--- gen/com/sanddproj/finlab/R.java
+++ gen/com/sanddproj/finlab/R.java
@@ -36,20 +36,23 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int radioButton1=0x7f070002;
         public static final int radioButton2=0x7f070001;
         public static final int radioSex=0x7f070000;
+        public static final int textView1=0x7f07000b;
     }
     public static final class layout {
         public static final int activity_fullscreen=0x7f030000;
+        public static final int activity_login=0x7f030001;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int comment_text_label=0x7f050008;
-        public static final int drugs_text_label=0x7f050006;
-        public static final int expence_checkbox=0x7f050002;
-        public static final int food_text_label=0x7f050005;
-        public static final int income_checkbox=0x7f050003;
-        public static final int other_text_label=0x7f050007;
-        public static final int road_text_label=0x7f050004;
-        public static final int submit_button=0x7f050001;
+        public static final int app_name=0x7f050001;
+        public static final int comment_text_label=0x7f050009;
+        public static final int drugs_text_label=0x7f050007;
+        public static final int expence_checkbox=0x7f050003;
+        public static final int food_text_label=0x7f050006;
+        public static final int income_checkbox=0x7f050004;
+        public static final int other_text_label=0x7f050008;
+        public static final int road_text_label=0x7f050005;
+        public static final int submit_button=0x7f050002;
+        public static final int welcome=0x7f050000;
     }
     public static final class style {
         /** 
